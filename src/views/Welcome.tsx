6da5c5c1eae794cf818673c6aa726d0412e73dab
hanyi319 <2387870957@qq.com>
2022-10-12 12:10:29 +0800

    feature: 添加 useSwipe

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,17 +1,25 @@
-import { defineComponent, h, Transition, VNode } from "vue";
+import { defineComponent, ref, Transition, VNode, watchEffect } from "vue";
 import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 import s from "./Welcome.module.scss";
 import logo from "../assets/icons/logo.svg";
+import { useSwipe } from "../hooks/useSwipe";
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
+    const main = ref<HTMLElement | null>(null);
+    const { direction, swiping } = useSwipe(main);
+
+    watchEffect(() => {
+      console.log(swiping.value, direction.value);
+    });
+
     return () => (
       <div class={s.wrapper}>
         <header>
           <img class={s.logo} src={logo} />
           <h1>清流记账</h1>
         </header>
-        <main class={s.main}>
+        <main class={s.main} ref={main}>
           <RouterView name="main">
             {({
               Component: X,
