cfb655ddf9d95c31946aeb67aa758973db6c7fae
hanyi319 <2387870957@qq.com>
2022-10-12 14:36:54 +0800

    feature: 滑动切换路由

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,15 +1,26 @@
 import s from "./welcome.module.scss";
-import { FunctionalComponent } from "vue";
+import { defineComponent, FunctionalComponent, ref, watchEffect } from "vue";
+import { useSwipe } from "../../hooks/useSwipe";
+import { useRouter } from "vue-router";
 
-export const First: FunctionalComponent = () => {
-  console.log(s.card);
+export const First = defineComponent({
+  setup() {
+    const div = ref<HTMLDivElement>();
+    const router = useRouter();
+    const { swiping, direction } = useSwipe(div, {
+      beforeStart: (e) => e.preventDefault(),
+    });
+    watchEffect(() => {
+      if (swiping.value && direction.value === "left") {
+        router.push("/welcome/2");
+      }
+    });
 
-  return (
-    <div class={s.card}>
-      <div class={s.title1}>开源节流</div>
-      <div class={s.content}>&nbsp;&nbsp;既会挣钱，也懂省钱。</div>
-    </div>
-  );
-};
-
-First.displayName = "First";
+    return () => (
+      <div class={s.card} ref={div}>
+        <div class={s.title1}>开源节流</div>
+        <div class={s.content}>&nbsp;&nbsp;既会挣钱，也懂省钱。</div>
+      </div>
+    );
+  },
+});
