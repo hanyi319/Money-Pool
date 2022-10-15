d416a42e932b010fec50daccbee8750a92b7b335
hanyi319 <2387870957@qq.com>
2022-10-15 15:28:32 +0800

    feature: 创建记账页面路由 /items 和 /items/create

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -9,6 +9,9 @@ import { Fourth } from "../components/welcome/Fourth";
 import { FourthActions } from "../components/welcome/FourthActions";
 import { Welcome } from "../views/Welcome";
 import { StartPage } from "../views/StartPage";
+import { ItemPage } from "../views/ItemPage";
+import { ItemList } from "../components/item/ItemList";
+import { ItemCreate } from "../components/item/ItemCreate";
 
 export const routes: RouteRecordRaw[] = [
   { path: "/", redirect: "/welcome" },
@@ -24,4 +27,12 @@ export const routes: RouteRecordRaw[] = [
     ],
   },
   { path: "/start", component: StartPage },
+  {
+    path: "/items",
+    component: ItemPage,
+    children: [
+      { path: "", component: ItemList },
+      { path: "create", component: ItemCreate },
+    ],
+  },
 ];
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, ref } from "vue";
+import { RouterLink } from "vue-router";
 import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
@@ -26,9 +27,13 @@ export const StartPage = defineComponent({
           <Icon name="note" class={s.icon} />
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button}>开始记账</Button>
+          <RouterLink to="/items/create">
+            <Button class={s.button}>开始记账</Button>
+          </RouterLink>
         </div>
-        <FloatButton iconName="add" />
+        <RouterLink to="/items/create">
+          <FloatButton iconName="add" />
+        </RouterLink>
         {refOverlayVisible.value && <Overlay onClose={() => (refOverlayVisible.value = false)} />}
       </div>
     );
