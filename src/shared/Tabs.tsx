8ca3348025f2938721831d52b46c5887f3821bc6
hanyi319 <2387870957@qq.com>
2022-10-16 14:06:04 +0800

    feature: 完成 Tabs 组件

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -12,10 +12,10 @@ export const Tabs = defineComponent({
   },
   setup: (props, context) => {
     return () => {
-      const array = context.slots.default?.();
-      if (!array) return () => null;
-      for (let i = 0; i < array.length; i++) {
-        if (array[i].type !== Tab) {
+      const tabs = context.slots.default?.();
+      if (!tabs) return () => null;
+      for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].type !== Tab) {
           throw new Error("<Tabs> only accepts <Tab> as children");
         }
       }
@@ -23,7 +23,7 @@ export const Tabs = defineComponent({
       return (
         <div class={s.tabs}>
           <ol class={s.tabs_nav}>
-            {array.map((item) => (
+            {tabs.map((item) => (
               <li
                 class={item.props?.name === props.selected ? s.selected : ""}
                 // onClick={() => props.onUpdateSelected?.(item.props?.name)}
@@ -33,6 +33,7 @@ export const Tabs = defineComponent({
               </li>
             ))}
           </ol>
+          <div>{tabs.find((item) => item.props?.name === props.selected)}</div>
         </div>
       );
     };
