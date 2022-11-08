3aeeb1a4eb24de26c903528908ff322330e43e3e
hanyi319 <2387870957@qq.com>
2022-11-08 14:56:29 +0800

    fix: 登录成功后保存 jwt 至 localStorage

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -8,6 +8,7 @@ import s from "./SignInPage.module.scss";
 import axios from "axios";
 import { http } from "../shared/Http";
 import { useBool } from "../hooks/useBool";
+import { history } from "../shared/history";
 
 export const SignInPage = defineComponent({
   setup: (props, context) => {
@@ -33,7 +34,9 @@ export const SignInPage = defineComponent({
         ])
       );
       if (!hasError(errors)) {
-        const response = await http.post("/session", formData);
+        const response = await http.post<{ jwt: string }>("/session", formData);
+        localStorage.setItem("jwt", response.data.jwt);
+        history.push("/");
       }
     };
     const onError = (error: any) => {
