4bbef7e4fdf08b856df55243745175868ef30bdb
hanyi319 <2387870957@qq.com>
2022-11-10 13:06:06 +0800

    feature: 创建 mock 模块

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,6 +1,10 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { mockSession } from "../mock/mock";
 
-type JSONValue = string | number | boolean | null | JSONValue[] | { [key: string]: JSONValue };
+type GetConfig = Omit<AxiosRequestConfig, "params" | "url" | "method">;
+type PostConfig = Omit<AxiosRequestConfig, "url" | "data" | "method">;
+type PatchConfig = Omit<AxiosRequestConfig, "url" | "data">;
+type DeleteConfig = Omit<AxiosRequestConfig, "params">;
 
 export class Http {
   instance: AxiosInstance;
@@ -9,41 +13,37 @@ export class Http {
       baseURL,
     });
   }
-  // read
-  get<R = unknown>(
-    url: string,
-    query?: Record<string, string>,
-    config?: Omit<AxiosRequestConfig, "params" | "url" | "method">
-  ) {
+  get<R = unknown>(url: string, query?: Record<string, JSONValue>, config?: GetConfig) {
     return this.instance.request<R>({ ...config, url: url, params: query, method: "get" });
   }
-  // create
-  post<R = unknown>(
-    url: string,
-    data?: Record<string, JSONValue>,
-    config?: Omit<AxiosRequestConfig, "url" | "data" | "method">
-  ) {
+  post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PostConfig) {
     return this.instance.request<R>({ ...config, url, data, method: "post" });
   }
-  // update
-  patch<R = unknown>(
-    url: string,
-    data?: Record<string, JSONValue>,
-    config?: Omit<AxiosRequestConfig, "url" | "data">
-  ) {
+  patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PatchConfig) {
     return this.instance.request<R>({ ...config, url, data, method: "patch" });
   }
-  // destroy
-  delete<R = unknown>(
-    url: string,
-    query?: Record<string, string>,
-    config?: Omit<AxiosRequestConfig, "params">
-  ) {
+  delete<R = unknown>(url: string, query?: Record<string, string>, config?: DeleteConfig) {
     return this.instance.request<R>({ ...config, url: url, params: query, method: "delete" });
   }
 }
 
 export const http = new Http("/api/v1");
+
+const mock = (response: AxiosResponse) => {
+  if (
+    location.hostname !== "localhost" &&
+    location.hostname !== "127.0.0.1" &&
+    location.hostname !== "192.168.3.57"
+  ) {
+    return false;
+  }
+  switch (response.config?.params?._mock) {
+    case "session":
+      [response.status, response.data] = mockSession(response.config);
+      return true;
+  }
+  return false;
+};
 
 http.instance.interceptors.request.use((config) => {
   const jwt = localStorage.getItem("jwt");
@@ -55,17 +55,27 @@ http.instance.interceptors.request.use((config) => {
 
 http.instance.interceptors.response.use(
   (response) => {
-    console.log("response");
+    mock(response);
     return response;
   },
+  (error) => {
+    if (mock(error.response)) {
+      return error.response;
+    } else {
+      throw error;
+    }
+  }
+);
+
+http.instance.interceptors.response.use(
+  (response) => response,
   (error) => {
     if (error.response) {
       const axiosError = error as AxiosError;
       if (axiosError.response?.status === 429) {
-        alert("请求过于频繁");
+        alert("你太频繁了");
       }
     }
     throw error;
-    // return Promise.reject(error)
   }
 );
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -38,7 +38,11 @@ export const SignInPage = defineComponent({
         ])
       );
       if (!hasError(errors)) {
-        const response = await http.post<{ jwt: string }>("/session", formData);
+        const response = await http
+          .post<{ jwt: string }>("/session", formData, {
+            params: { _mock: "session" },
+          })
+          .catch(onError);
         localStorage.setItem("jwt", response.data.jwt);
         // router.push("/sign_in?return_to=" + encodeURIComponent(route.fullPath));
         const returnTo = route.query.return_to?.toString();
