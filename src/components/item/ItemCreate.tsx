a017029c0d810a3ebd713933829598cf6b11e5d3
韩毅 <2387870957@qq.com>
2022-11-19 17:16:24 +0800

    wip: 加载更多标签

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, onMounted, PropType, ref } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
+import { Button } from "../../shared/Button";
 import { http } from "../../shared/Http";
 import { Icon } from "../../shared/Icon";
 import { Tabs, Tab } from "../../shared/Tabs";
@@ -13,13 +14,19 @@ export const ItemCreate = defineComponent({
     },
   },
   setup: (props, context) => {
-    const refKind = ref("支出");
+    const refKind = ref("支出"); // 交易类别，默认为「支出」
+    const refPage = ref(0); // 当前展示标签的页数，默认为 0（也符合未加载时的状态）
+    const refHasMore = ref(false); // 是否要加载更多标签，默认为不加载
     onMounted(async () => {
-      const response = await http.get<{ resources: Tag[] }>("/tags", {
+      const response = await http.get<Resources<Tag>>("/tags", {
         kind: "expenses",
         _mock: "tagIndex",
       });
-      refExpensesTags.value = response.data.resources;
+      const { resources, pager } = response.data;
+      // 因为第 1 次请求没有传 page 参数，所以默认只创建 25 个标签，也就是只有 1 页
+      refExpensesTags.value = resources;
+      // 比如总共要展示 26 个标签，因为最开始是第 1 页，所以就是 (1 - 1) * 25 + 25 < 26，为 true
+      refHasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count;
     });
     const refExpensesTags = ref<Tag[]>([]);
     onMounted(async () => {
@@ -30,7 +37,6 @@ export const ItemCreate = defineComponent({
       refIncomeTags.value = response.data.resources;
     });
     const refIncomeTags = ref<Tag[]>([]);
-
     return () => (
       <MainLayout class={s.layout}>
         {{
@@ -41,19 +47,28 @@ export const ItemCreate = defineComponent({
               {/* <Tabs selected={refKind.value} onUpdateSelected={(name) => (refKind.value = name)}> */}
               <div class={s.wrapper}>
                 <Tabs v-model:selected={refKind.value} class={s.tabs}>
-                  <Tab name="支出" class={s.tags_wrapper}>
-                    <div class={s.tag}>
-                      <div class={s.icon_wrapper}>
-                        <Icon name="add" class={s.createTag} />
+                  <Tab name="支出">
+                    <div class={s.tags_wrapper}>
+                      <div class={s.tag}>
+                        <div class={s.icon_wrapper}>
+                          <Icon name="add" class={s.createTag} />
+                        </div>
+                        <div class={s.name}>新增</div>
                       </div>
-                      <div class={s.name}>新增</div>
+                      {refExpensesTags.value.map((tag) => (
+                        <div class={[s.tag, s.selected]}>
+                          <div class={s.sign}>{tag.sign}</div>
+                          <div class={s.name}>{tag.name}</div>
+                        </div>
+                      ))}
                     </div>
-                    {refExpensesTags.value.map((tag) => (
-                      <div class={[s.tag, s.selected]}>
-                        <div class={s.sign}>{tag.sign}</div>
-                        <div class={s.name}>{tag.name}</div>
-                      </div>
-                    ))}
+                    <div class={s.more}>
+                      {refHasMore ? (
+                        <Button class={s.loadMore}>加载更多</Button>
+                      ) : (
+                        <span class={s.noMore}>暂无更多</span>
+                      )}
+                    </div>
                   </Tab>
                   <Tab name="收入" class={s.tags_wrapper}>
                     <div class={s.tag}>
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -15,12 +15,23 @@ export const mockSession: Mock = (config) => {
 };
 
 export const mockTagIndex: Mock = (config) => {
+  const { kind, page } = config.params; // 从请求的配置参数解构出收支类别、当前页数
+  const per_page = 25; // 每页只展示 25 个标签
+  const count = 26; // 为了测试加载更多标签功能，构造 26 个标签数据，也就是需要两页展示全部标签
   let id = 0;
   const createId = () => {
     id += 1;
     return id;
   };
+  // 创建一个与「加载更多标签」相关的对象
+  const createPager = (page = 1) => ({
+    page, // 当前展示标签的页数，默认为第 1 页
+    per_page, // 每页能展示的标签个数
+    count, // 常用标签列表的总个数
+  });
+  // 创建一个包含所有标签的数组
   const createTag = (n = 1, attrs?: any) =>
+    // 构造一个长度为 n 的数组（默认为 1）
     Array.from({ length: n }).map(() => ({
       id: createId(),
       name: faker.lorem.word(),
@@ -28,8 +39,14 @@ export const mockTagIndex: Mock = (config) => {
       kind: config.params.kind,
       ...attrs,
     }));
-  if (config.params.kind === "expenses") {
-    return [200, { resources: createTag(7) }];
+  const createBody = (n = 1, attrs?: any) => ({
+    resources: createTag(n),
+    pager: createPager(page),
+  });
+  if (kind === "expenses" && (!page || page === 1)) {
+    return [200, createBody(25)]; // 支出标签的第 1 页
+  } else if (kind === "expenses" && page === 2) {
+    return [200, createBody(1)]; // 支出标签的第 2 页
   } else {
     return [200, { resources: createTag(20) }];
   }
