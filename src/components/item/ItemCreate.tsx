3634d3e9540090ddf158991e8474c10b55aa70b8
韩毅 <2387870957@qq.com>
2022-11-20 15:38:27 +0800

    feature: 封装 Tags 组件

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,10 +1,8 @@
-import { defineComponent, onMounted, PropType, ref } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
-import { Button } from "../../shared/Button";
-import { http } from "../../shared/Http";
 import { Icon } from "../../shared/Icon";
 import { Tabs, Tab } from "../../shared/Tabs";
-import { useTags } from "../../shared/useTags";
+import { Tags } from "./Tags";
 import { InputPad } from "./InputPad";
 import s from "./ItemCreate.module.scss";
 
@@ -16,35 +14,6 @@ export const ItemCreate = defineComponent({
   },
   setup: (props, context) => {
     const refKind = ref("支出"); // 交易类别，默认为「支出」
-    /**
-     * 使用 useTags 结构出：
-     * 标签数组、是否要加载更多标签、加载标签函数
-     *
-     * 并给 useTags 传入一个请求函数，相关配置参数为：
-     * 交易类别、当前展示标签的页数、调用哪种 mock 函数
-     */
-    const {
-      tags: expensesTags, // 为了避免和支出标签数组重复，取一个别名
-      hasMore: expensesHasMore,
-      fetchTags: expensesFetchTags,
-    } = useTags((page) => {
-      return http.get<Resources<Tag>>("/tags", {
-        kind: "expenses",
-        page: page + 1, // 注意需要 +1，比如第一次请求时，page 为 0，需要在这里 +1 才能加载第 1 页的标签
-        _mock: "tagIndex",
-      });
-    });
-    const {
-      tags: incomeTags,
-      hasMore: incomeHasMore,
-      fetchTags: incomeFetchTags,
-    } = useTags((page) => {
-      return http.get<Resources<Tag>>("/tags", {
-        kind: "income",
-        page: page + 1,
-        _mock: "tagIndex",
-      });
-    });
     return () => (
       <MainLayout class={s.layout}>
         {{
@@ -55,54 +24,10 @@ export const ItemCreate = defineComponent({
               <div class={s.wrapper}>
                 <Tabs v-model:selected={refKind.value} class={s.tabs}>
                   <Tab name="支出">
-                    <div class={s.tags_wrapper}>
-                      <div class={s.tag}>
-                        <div class={s.icon_wrapper}>
-                          <Icon name="add" class={s.createTag} />
-                        </div>
-                        <div class={s.name}>新增</div>
-                      </div>
-                      {expensesTags.value.map((tag) => (
-                        <div class={[s.tag, s.selected]}>
-                          <div class={s.sign}>{tag.sign}</div>
-                          <div class={s.name}>{tag.name}</div>
-                        </div>
-                      ))}
-                    </div>
-                    <div class={s.more}>
-                      {expensesHasMore ? (
-                        <Button class={s.loadMore} onClick={expensesFetchTags}>
-                          加载更多
-                        </Button>
-                      ) : (
-                        <span class={s.noMore}>暂无更多</span>
-                      )}
-                    </div>
+                    <Tags kind="expenses" />
                   </Tab>
                   <Tab name="收入">
-                    <div class={s.tags_wrapper}>
-                      <div class={s.tag}>
-                        <div class={s.icon_wrapper}>
-                          <Icon name="add" class={s.createTag} />
-                        </div>
-                        <div class={s.name}>新增</div>
-                      </div>
-                      {incomeTags.value.map((tag) => (
-                        <div class={[s.tag, s.selected]}>
-                          <div class={s.sign}>{tag.sign}</div>
-                          <div class={s.name}>{tag.name}</div>
-                        </div>
-                      ))}
-                    </div>
-                    <div class={s.more}>
-                      {incomeHasMore ? (
-                        <Button class={s.loadMore} onClick={incomeFetchTags}>
-                          加载更多
-                        </Button>
-                      ) : (
-                        <span class={s.noMore}>暂无更多</span>
-                      )}
-                    </div>
+                    <Tags kind="income" />
                   </Tab>
                 </Tabs>
                 <div class={s.inputPad_wrapper}>
---
+++ src/components/item/Tags.tsx
@@ -0,0 +1,57 @@
+import { defineComponent, PropType } from "vue";
+import { http } from "../../shared/Http";
+import { Icon } from "../../shared/Icon";
+import { Button } from "../../shared/Button";
+import { useTags } from "../../shared/useTags";
+import s from "./Tags.module.scss";
+
+export const Tags = defineComponent({
+  props: {
+    kind: {
+      type: String as PropType<string>,
+    },
+  },
+  setup: (props, context) => {
+    /**
+     * 使用 useTags 结构出：
+     * 标签数组、是否要加载更多标签、加载标签函数
+     *
+     * 并给 useTags 传入一个请求函数，相关配置参数为：
+     * 交易类别、当前展示标签的页数、调用哪种 mock 函数
+     */
+    const { tags, hasMore, fetchTags } = useTags((page) => {
+      return http.get<Resources<Tag>>("/tags", {
+        kind: props.kind,
+        page: page + 1, // 注意需要 +1，比如第一次请求时，page 为 0，需要在这里 +1 才能加载第 1 页的标签
+        _mock: "tagIndex",
+      });
+    });
+    return () => (
+      <>
+        <div class={s.tags_wrapper}>
+          <div class={s.tag}>
+            <div class={s.icon_wrapper}>
+              <Icon name="add" class={s.createTag} />
+            </div>
+            <div class={s.name}>新增</div>
+          </div>
+          {tags.value.map((tag) => (
+            <div class={[s.tag, s.selected]}>
+              <div class={s.sign}>{tag.sign}</div>
+              <div class={s.name}>{tag.name}</div>
+            </div>
+          ))}
+        </div>
+        <div class={s.more}>
+          {hasMore ? (
+            <Button class={s.loadMore} onClick={fetchTags}>
+              加载更多
+            </Button>
+          ) : (
+            <span class={s.noMore}>暂无更多</span>
+          )}
+        </div>
+      </>
+    );
+  },
+});
