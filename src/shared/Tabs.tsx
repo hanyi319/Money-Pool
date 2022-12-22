f774493cfab323bbf3d5c110b352316c0f15eabd
韩毅 <2387870957@qq.com>
2022-12-22 16:11:11 +0800

    fix: 优化 Tabs 组件切换效果

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, PropType } from "vue";
+import { defineComponent, onMounted, PropType, ref, onUpdated } from "vue";
 import s from "./Tabs.module.scss";
 
 export const Tabs = defineComponent({
@@ -16,6 +16,24 @@ export const Tabs = defineComponent({
   },
   emits: ["update:selected"],
   setup: (props, context) => {
+    // 设置导航条
+    const container = ref<HTMLDivElement>();
+    const selectedItem = ref<HTMLDivElement>();
+    const indicator = ref<HTMLDivElement>();
+    const toggleNav = () => {
+      // 动态设置导航条宽度
+      const { width } = selectedItem.value!.getBoundingClientRect();
+      indicator.value!.style.width = width + "px";
+
+      // 动态设置导航条位置
+      const { left: left1 } = container.value!.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value!.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value!.style.left = left + "px";
+    };
+    onMounted(toggleNav);
+    onUpdated(toggleNav);
+
     return () => {
       const tabs = context.slots.default?.();
       if (!tabs) return () => null;
@@ -26,20 +44,24 @@ export const Tabs = defineComponent({
       }
       const cp = props.classPrefix;
       return (
-        <div class={[s.tabs, cp + "_tabs"]}>
-          <ol class={[s.tabs_nav, cp + "_tabs_nav"]}>
-            {tabs.map((item) => (
-              <li
-                class={[
-                  item.props?.value === props.selected ? [s.selected, cp + "_selected"] : "",
-                  cp + "_tabs_nav_item",
-                ]}
-                onClick={() => context.emit("update:selected", item.props?.value)}
-              >
-                {item.props?.name}
-              </li>
-            ))}
-          </ol>
+        <div class={[s.tabs_wrapper, cp + "_tabs"]}>
+          <div class={s.tabs}>
+            <ol class={[s.tabs_nav, cp + "_tabs_nav"]} ref={container}>
+              {tabs.map((item) => (
+                <li
+                  class={[
+                    item.props?.value === props.selected ? [s.selected, cp + "_selected"] : "",
+                    cp + "_tabs_nav_item",
+                  ]}
+                  onClick={() => context.emit("update:selected", item.props?.value)}
+                  ref={item.props?.value === props.selected ? selectedItem : ""}
+                >
+                  {item.props?.name}
+                </li>
+              ))}
+            </ol>
+            <div class={s.indicator} ref={indicator}></div>
+          </div>
           {props.reRenderOnSelect ? (
             // 使用 key，避免缓存
             <div key={props.selected}>
